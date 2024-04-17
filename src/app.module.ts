7b7cbbf244630efec06856e26c6550abc7fa9252
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2024-04-17 09:49:10 +0200

    refactored prismaModule to shared just one instancy of prismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [ProductsModule, UsersModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import {
   ConflictException,
   NotFoundException,
 } from '@nestjs/common';
-import { PrismaService } from 'src/shared/services/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { User, Password } from '@prisma/client';
 import { error } from 'console';
 
