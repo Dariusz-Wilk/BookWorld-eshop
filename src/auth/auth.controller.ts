80844b8944a5a6d32965bbed22c75b22d2f1abd1
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2024-04-17 10:46:47 +0200

    added registration new user - endpoint & function with res

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,31 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDTO } from './dtos/register.dto';
+import { Post, Body, HttpException, HttpStatus } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  async register(@Body() registerData: RegisterDTO) {
+    try {
+      const newUser = await this.authService.register(registerData);
+
+      return {
+        statusCode: HttpStatus.CREATED,
+        message: 'Nowy użytkownik został pomyślnie dodany.',
+        data: newUser,
+      };
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Wystąpił błąd podczas dodawania nowego użytkownika.',
+          errorDetails: error.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { RegisterDTO } from './dtos/register.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  public async register(registrationData: RegisterDTO) {
+    const hashedPassword = await bcrypt.hash(registrationData.password, 10);
+    const userData = {
+      email: registrationData.email,
+      name: registrationData.name,
+      address: registrationData.address,
+    };
+
+    return await this.usersService.addNewUser(userData, hashedPassword);
+  }
 }
