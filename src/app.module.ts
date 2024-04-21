ab1ef8edbef4ba0370ebbc6d2f92e98ec3add615
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2024-04-21 14:35:19 +0200

    refactored app to hide private data

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ProductsModule, UsersModule, AuthModule, PrismaModule],
+  imports: [
+    ProductsModule,
+    UsersModule,
+    AuthModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser());
   app.setGlobalPrefix('api');
 
   await app.enableShutdownHooks();
 
-  await app.listen(8000);
+  await app.listen(configService.get('port'));
 }
 bootstrap();
